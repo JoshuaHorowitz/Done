f3c3d3f5fb9c0cbccc576f9d837746cb614300d3
JoshuaHorowitz <57075822+JoshuaHorowitz@users.noreply.github.com>
2022-11-06 15:26:22 -0500

    added pubsec.yaml

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:done/main.dart';
+import 'package:todoflutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
